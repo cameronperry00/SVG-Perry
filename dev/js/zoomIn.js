150712c1c4af60a987c9705a19f283ec54842d21
cameronperry00 <63419233+cameronperry00@users.noreply.github.com>
2021-04-27 15:10:27 -0400

    commit

--- dev/js/zoomIn.js
+++ dev/js/zoomIn.js
@@ -8,8 +8,8 @@ gsap.set(".front-mtns",{transformOrigin:"center"});
 gsap.set(".trees",{transformOrigin:"center"});
 
 
-zoomTL.from("#orange-mtn",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
-            .from("#red-mtn",{duration:5.75, scale:10,ease: "power4.out",y:"+=800"},"zoom")
+zoomTL.from("#mountain1",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
+            .from("#mountain2",{duration:5.75, scale:10,ease: "power4.out",y:"+=800"},"zoom")
             .from(".front-mtns",{duration:5.5, scale:10,ease: "power4.out",y:"+=800"},"zoom")
             .from("#trees-5",{duration:5.5, scale:10,ease: "power4.out",y:"+=420"},"zoom")
             .from("#trees-4",{duration:5.25, scale:10,ease: "power4.out",y:"+=420"},"zoom")
