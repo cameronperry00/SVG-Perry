39a0bb7fdb2d021a6254145a1a394bedefb62d83
cameronperry00 <63419233+cameronperry00@users.noreply.github.com>
2021-05-06 15:17:09 -0400

    commit

--- dev/js/fadeIn.js
+++ dev/js/fadeIn.js
@@ -3,4 +3,4 @@ import { gsap } from "gsap";
 export let fadeInTL = gsap.timeline();
 fadeInTL.from("#sky_3_",{alpha:0, duration:5, scale:20})
         .from("#clouds_3_",{alpha:0, stagger:0.25, duration:0.5},"-=2")
-        .from("#moon",{alpha:0, stagger:0.25, duration:0.5},"-=2");
+        .from("#moon",{alpha:0, stagger:1, duration:0.5},"-=2");
