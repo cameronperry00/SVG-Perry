69353bc296368e5582f5cd5d9401e04cdc39135f
cameronperry00 <63419233+cameronperry00@users.noreply.github.com>
2021-05-11 15:18:57 -0400

    commit

--- dev/js/takeOff.js
+++ dev/js/takeOff.js
@@ -1,13 +1,13 @@
 import { gsap } from "gsap";
 
-gsap.set("#rocket",{transformOrigin:"center"});
+gsap.set("#rocket-2",{transformOrigin:"center"});
 
 export let takeOffTL = gsap.timeline();
 
-takeOffTL.to("#rocket",{duration:0.15, rotation:-5,ease:"none"})
-        .to("#rocket",{duration:0.15, rotation:5, yoyo:true, repeat:8,ease:"none"})
-        .to("#rocket",{duration:0.15, rotation:0,ease:"none"})
-        .to("#rocket",{duration:9, y:"-=700", ease: "power4.in"}, "upAndAway")
+takeOffTL.to("#rocket-2",{duration:0.15, rotation:-5,ease:"none"})
+        .to("#rocket-2",{duration:0.15, rotation:5, yoyo:true, repeat:8,ease:"none"})
+        .to("#rocket-2",{duration:0.15, rotation:0,ease:"none"})
+        .to("#rocket-2",{duration:9, y:"-=700", ease: "power4.in"}, "upAndAway")
         .to("#clouds-2",{duration:6, y:"1000", ease: "power4.in"}, "upAndAway")
         .to("#trees1-2",{duration:6, y:"1000", ease: "power4.in"}, "upAndAway")
         .to("#trees2-2",{duration:6, y:"1000", ease: "power4.in"},"upAndAway")
