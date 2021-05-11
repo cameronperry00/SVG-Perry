155ec3f727bd2d5bd4511bac9001deb4574329e5
cameronperry00 <63419233+cameronperry00@users.noreply.github.com>
2021-05-11 14:40:40 -0400

    commit

--- dev/js/takeOff.js
+++ dev/js/takeOff.js
@@ -1,17 +1,17 @@
 import { gsap } from "gsap";
 
-gsap.set("#rocket",{transformOrigin:"center"});
+gsap.set("#rocket-2",{transformOrigin:"center"});
 
 export let takeOffTL = gsap.timeline();
 
-takeOffTL.to("#rocket_1_",{duration:0.15, rotation:-10,ease:"none"})
-        .to("#rocket_1_",{duration:0.15, rotation:10, yoyo:true, repeat:10,ease:"none"})
-        .to("#rocket_1_",{duration:0.15, rotation:0,ease:"none"})
-        .to("#rocket_1_",{duration:7, y:"-=800", ease: "power3.in"}, "upAndAway")
-        .to("#clouds_3_",{duration:6, y:"1000", ease: "power4.in"}, "upAndAway")
-        .to("#trees1_2_",{duration:6, y:"1000", ease: "power4.in"}, "upAndAway")
-        .to("#trees2_2_",{duration:6, y:"1000", ease: "power4.in"},"upAndAway")
+takeOffTL.to("#rocket-2",{duration:0.15, rotation:-10,ease:"none"})
+        .to("#rocket-2",{duration:0.15, rotation:10, yoyo:true, repeat:10,ease:"none"})
+        .to("#rocket-2",{duration:0.15, rotation:0,ease:"none"})
+        .to("#rocket-2",{duration:7, y:"-=800", ease: "power3.in"}, "upAndAway")
+        .to("#clouds-2",{duration:6, y:"1000", ease: "power4.in"}, "upAndAway")
+        .to("#trees1-2",{duration:6, y:"1000", ease: "power4.in"}, "upAndAway")
+        .to("#trees2-2",{duration:6, y:"1000", ease: "power4.in"},"upAndAway")
         .to("#mountain1",{duration:6, y:"1000", ease: "power4.in"},"upAndAway")
         .to("#mountain2",{duration:6, y:"1000", ease: "power4.in"},"upAndAway")
-        .to("#ground_3_",{duration:6, y:"1000", ease: "power4.in"},"upAndAway")
-        .to("#moon",{duration:6, y:"1000", ease: "power4.in"},"upAndAway");
+        .to("#ground-2",{duration:6, y:"1000", ease: "power4.in"},"upAndAway")
+        .to("#moon-2",{duration:6, y:"1000", ease: "power4.in"},"upAndAway")
