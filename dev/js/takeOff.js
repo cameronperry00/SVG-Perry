1e4ecbf0d24279153ea5405e7fb7ca94bc828c6d
cameronperry00 <63419233+cameronperry00@users.noreply.github.com>
2021-05-10 22:34:21 -0400

    commit

--- dev/js/takeOff.js
+++ dev/js/takeOff.js
@@ -1,18 +1,12 @@
 import { gsap } from "gsap";
 
 gsap.set("#rocket",{transformOrigin:"center"});
-gsap.set("#smoke-1",{transformOrigin:"center bottom"});
-gsap.set("#smoke-1",{transformOrigin:"center bottom"});
-// gsap.set("#smoke-3",{display:"none"});
-// gsap.set(".smoke",{display:"none"});
 
 export let takeOffTL = gsap.timeline();
 
 takeOffTL.to("#rocket_1_",{duration:0.15, rotation:-10,ease:"none"})
         .to("#rocket_1_",{duration:0.15, rotation:10, yoyo:true, repeat:10,ease:"none"})
         .to("#rocket_1_",{duration:0.15, rotation:0,ease:"none"})
-        .from("#smoke-1",{duration:3.5, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"})
-        .from("#smoke-2",{duration:4, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"})
         .to("#rocket_1_",{duration:5, y:"-=700", ease: "power3.in"})
         .to("#clouds",{duration:5, y:"+=700", ease: "power3.in"})
         .to("#front",{duration:5, y:"+=600", ease: "power3.in"})
