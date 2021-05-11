85d076ad5e31817fc290bbaa8f0df198888caf7c
cameronperry00 <63419233+cameronperry00@users.noreply.github.com>
2021-05-10 21:16:05 -0400

    commit

--- dev/js/takeOff.js
+++ dev/js/takeOff.js
@@ -1,20 +1,20 @@
 import { gsap } from "gsap";
 
-gsap.set("#space-ship",{transformOrigin:"center"});
+gsap.set("#rocket",{transformOrigin:"center"});
 gsap.set("#smoke-1",{transformOrigin:"center bottom"});
 gsap.set("#smoke-1",{transformOrigin:"center bottom"});
 // gsap.set("#smoke-3",{display:"none"});
 // gsap.set(".smoke",{display:"none"});
 
-export let liftOffTL = gsap.timeline();
+export let takeOffTL = gsap.timeline();
 
-liftOffTL.to("#space-ship",{duration:0.15, rotation:-10,ease:"none"})
-        .to("#space-ship",{duration:0.15, rotation:10, yoyo:true, repeat:10,ease:"none"})
-        .to("#space-ship",{duration:0.15, rotation:0,ease:"none", onComplete:controlFlames})
-        .from("#smoke-1",{duration:3.5, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"},'upAndAway')
-        .from("#smoke-2",{duration:4, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"},'upAndAway')
-        .to("#space-ship",{duration:5, y:"-=700", ease: "power3.in"},"upAndAway")
-        .to("#clouds",{duration:5, y:"+=700", ease: "power3.in"},'upAndAway')
-        .to("#front",{duration:5, y:"+=600", ease: "power3.in"},'upAndAway')
-        .to("#middle",{duration:5, y:"+=500", ease: "power3.in"},'upAndAway')
-        .from("#space",{duration:5,alpha:0, ease: "power3.in"},'upAndAway');
+liftOffTL.to("#rocket",{duration:0.15, rotation:-10,ease:"none"})
+        .to("#rocket",{duration:0.15, rotation:10, yoyo:true, repeat:10,ease:"none"})
+        .to("#rocket",{duration:0.15, rotation:0,ease:"none", onComplete:controlFlames})
+        .from("#smoke-1",{duration:3.5, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"})
+        .from("#smoke-2",{duration:4, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"})
+        .to("#rocket",{duration:5, y:"-=700", ease: "power3.in"})
+        .to("#clouds",{duration:5, y:"+=700", ease: "power3.in"})
+        .to("#front",{duration:5, y:"+=600", ease: "power3.in"})
+        .to("#middle",{duration:5, y:"+=500", ease: "power3.in"})
+        .from("#space",{duration:5,alpha:0, ease: "power3.in"});
