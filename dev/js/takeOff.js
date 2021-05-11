7d30493009244f5101bb8aee75f5a479ae475497
cameronperry00 <63419233+cameronperry00@users.noreply.github.com>
2021-05-10 21:36:09 -0400

    commit

--- dev/js/takeOff.js
+++ dev/js/takeOff.js
@@ -8,12 +8,12 @@ gsap.set("#smoke-1",{transformOrigin:"center bottom"});
 
 export let takeOffTL = gsap.timeline();
 
-// takeOffTL.to("#rocket",{duration:0.15, rotation:-10,ease:"none"})
-//         .to("#rocket",{duration:0.15, rotation:10, yoyo:true, repeat:10,ease:"none"})
-//         .to("#rocket",{duration:0.15, rotation:0,ease:"none", onComplete:controlFlames})
-//         .from("#smoke-1",{duration:3.5, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"})
-//         .from("#smoke-2",{duration:4, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"})
-//         .to("#rocket",{duration:5, y:"-=700", ease: "power3.in"})
-//         .to("#clouds",{duration:5, y:"+=700", ease: "power3.in"})
-//         .to("#front",{duration:5, y:"+=600", ease: "power3.in"})
-//         .to("#middle",{duration:5, y:"+=500", ease: "power3.in"})
+takeOffTL.to("#rocket_1_",{duration:0.15, rotation:-10,ease:"none"})
+        .to("#rocket_1_",{duration:0.15, rotation:10, yoyo:true, repeat:10,ease:"none"})
+        .to("#rocket_1_",{duration:0.15, rotation:0,ease:"none", onComplete:controlFlames})
+        .from("#smoke-1",{duration:3.5, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"})
+        .from("#smoke-2",{duration:4, y:"+=150", scale:.75, alpha:0.5, ease: "power3.in"})
+        .to("#rocket_1_",{duration:5, y:"-=700", ease: "power3.in"})
+        .to("#clouds",{duration:5, y:"+=700", ease: "power3.in"})
+        .to("#front",{duration:5, y:"+=600", ease: "power3.in"})
+        .to("#middle",{duration:5, y:"+=500", ease: "power3.in"})
